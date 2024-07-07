914762d2089405636a800b3d63e23ec46056777d
juan-esteban-berger <juanestebanberger@gmail.com>
2024-07-07 21:40:57 +0000

    fixed tableau embed

--- dashboard_tableau/app.py
+++ dashboard_tableau/app.py
@@ -3,23 +3,19 @@ warnings.filterwarnings("ignore")
 
 import streamlit as st
 import streamlit.components.v1 as components
-# wide mode
-st.set_page_config(layout="wide",
-                   page_title="Tableau Dashboard",
-                   initial_sidebar_state="collapsed")
+st.set_page_config(layout="wide",page_title="Tableau Dashboard",initial_sidebar_state="collapsed")
 
+# Make sure container is centered
 st.markdown("""
 <style>
-        [data-testid="stDecoration"] {
-                display: none;
-        }
+    [data-testid="stDecoration"] {display: none;}
     #MainMenu {visibility: hidden;}
     footer {visibility: hidden;}
+
 </style>""",
 unsafe_allow_html=True)
 
 with open('tableau_embed.html', 'r') as f:
     tableau_html = f.read()
 
-# Use the html method because the iframe method cannot display local html files
-components.html(tableau_html, height=1200)
+components.html(tableau_html, height=1140, scrolling=False)
